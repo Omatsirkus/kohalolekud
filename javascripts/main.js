12fbea87732465872dd5a23dae1a53b4bf5ba43f
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-03-10 12:38:02 +0200

    test

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,34 @@
-console.log('This would be the main JS file.');
+var configuration = {
+  "language": "estonian",
+  "cards": ["groups", "mark"],
+  "ENTU_URI": 'https://omatsirkus.entu.ee/',
+  "kohalolekud_eid": 652
+}
+
+configuration['ENTU_API'] = configuration.ENTU_URI + 'api2/'
+configuration['ENTU_API_AUTH'] = configuration.ENTU_API + 'user/auth'
+configuration['ENTU_API_USER'] = configuration.ENTU_API + 'user'
+configuration['ENTU_API_ENTITY'] = configuration.ENTU_API + 'entity'
+configuration['ENTU_API_POST_FILE'] = configuration.ENTU_API + 'file'
+
+
+
+    // console.log($('.checkbox'))
+
+    $.get( configuration['ENTU_API_ENTITY'] + '?definition=group' )
+        .done(function fetchGroupsOk( data ) {
+            console.log(data)
+            data.result.forEach(function iterateGroups(entu_group) {
+                console.log(entu_group)
+                var checkbox_div = $('<div for="CB_' + entu_group.id + '" class="checkbox"/>')
+                var checkbox_label = $('<label>' + entu_group.name + '</label>')
+                var checkbox_input = $('<input type="checkbox" id="CB_' + entu_group.id + '" eid="' + entu_group.id + '" value=""/>')
+                checkbox_label.prepend(checkbox_input)
+                checkbox_div.append(checkbox_label)
+                checkbox_input.on('change', function() {
+                            console.log(checkbox_input)
+                        })
+                $('#select_groups').append(checkbox_div)
+            })
+
+        })
